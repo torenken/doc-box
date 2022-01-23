e68529a37e74f8e5c0da848235e368713d258afd
Thomas Renken <torenken@gmail.com>
2022-01-23 01:07:52 +0100

    add integration-tests for loader

---
+++ pkg/store/doc/cmd/loader_test.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+
+	"github.com/torenken/doc-box/pkg/cfg"
+	"github.com/torenken/doc-box/pkg/store/doc/domain"
+)
+
+func TestGetterIntegration(t *testing.T) {
+	if _, b := os.LookupEnv("LOCALSTACK"); !b {
+		t.Skip("Start LocalStack Environment to run this test")
+	}
+	logger := zap.S()
+
+	_ = os.Setenv("DOCUMENT_TABLE_NAME", "docbox-document")
+
+	t.Run("load document by doc id", func(t *testing.T) {
+		ddb := cfg.NewLocalDynamoDB("http://127.0.0.1:4566")
+
+		doc, err := NewLoader(ddb, logger).Load(context.TODO(), "4b69ddf3-69fe-481c-a4e5-4e54456f6284")
+		assert.NoError(t, err)
+		assert.Equal(t, "4b69ddf3-69fe-481c-a4e5-4e54456f6284", doc.Id)
+	})
+
+	t.Run("empty document when no task with id", func(t *testing.T) {
+		ddb := cfg.NewLocalDynamoDB("http://127.0.0.1:4566")
+
+		doc, err := NewLoader(ddb, logger).Load(context.TODO(), "no-doc-id")
+		assert.NoError(t, err)
+		assert.Equal(t, domain.Document{}, doc)
+	})
+}
