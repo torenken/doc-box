089b4c70e0c0986edaa20ffcbe3c0e976faa6816
Thomas Renken <torenken@gmail.com>
2023-03-16 11:52:48 +0100

    add iac tests

--- deploy/cdk/.projenrc.js
+++ deploy/cdk/.projenrc.js
@@ -33,6 +33,11 @@ const project = new awscdk.AwsCdkTypeScriptApp({
 });
 
 project.setScript('postinstall', 'touch node_modules/go.mod'); //This step is necessary so that go mod tidy ignores the cdk-go deps.
-project.setScript('audit:level-high', '/bin/bash -c \'yarn audit; [[ $? -ge 8 ]] && exit 1 || exit 0\'');
+
+project.setScript('audit:check', 'yarn audit:level-high && yarn audit:level-critical');
+project.setScript('audit:level-high', '/bin/bash -c \'yarn audit --groups dependencies; [[ $? -ge 8 ]] && exit 1 || exit 0\'');
+project.setScript('audit:level-critical', '/bin/bash -c \'yarn audit --groups devDependencies; [[ $? -ge 16 ]] && exit 1 || exit 0\'');
+
+project.setScript('snyk:iac', 'cd cdk.out && snyk iac test --policy-path=../.snyk --severity-threshold=high --report');
 
 project.synth();
